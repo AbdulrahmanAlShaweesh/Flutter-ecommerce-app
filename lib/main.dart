487ff8fd1d46ca2caf8f7deb36cb451d3abc148f
Alshwish Abdulrahman <alshwishabdulrahman@Alshwishs-MacBook-Pro.local>
2023-08-06 19:09:55 +0800

    inital the app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    EcommerceApp(),
+  );
+}
+
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
